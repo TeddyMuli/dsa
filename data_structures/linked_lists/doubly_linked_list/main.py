ef9857a99f9e9094c6491ee1e127e4fe3cac8aad
TeddyMuli <tmuli974@gmail.com>
2025-02-06 11:16:52 +0300

    create node class

--- data_structures/linked_lists/doubly_linked_list/main.py
+++ data_structures/linked_lists/doubly_linked_list/main.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python3
 ''' Python module for double linked lists
 '''
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
