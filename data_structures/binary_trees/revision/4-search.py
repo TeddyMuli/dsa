562ae345b088365b7dbaf460d52b43b40d893844
TeddyMuli <tmuli974@gmail.com>
2025-07-29 18:28:25 +0300

    implement search

--- data_structures/binary_trees/revision/4-search.py
+++ data_structures/binary_trees/revision/4-search.py
@@ -1,1 +1,55 @@
 #!/usr/bin/python3
+class BinaryTree:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def search(self, node, value):
+        if node is None:
+            return False
+        if node.data == value:
+            print(f"\n{value} is in the binary tree")
+            return True
+        found_left = self.search(node.left, value)
+        found_right = self.search(node.right, value)
+        if not found_left and not found_right and node == self:
+            print(f"{value} is NOT in the binary tree")
+        return found_left or found_right
+
+    def insert(self, data):
+        if self is None:
+            self.data = data
+            return
+        
+        queue = [self]
+        while queue:
+            node = queue.pop(0)
+            if node.left is None:
+                node.left = BinaryTree(data)
+                break
+            else:
+                queue.append(node.left)
+
+            if node.right is None:
+                node.right = BinaryTree(data)
+                break
+            else:
+                queue.append(node.right)
+
+    def pre_order(self, node):
+        if node is None:
+            return
+        
+        print(node.data, end=' ')
+        self.pre_order(node.left)
+        self.pre_order(node.right)
+
+if __name__ == "__main__":
+    root = BinaryTree(1)
+    root.insert(2)
+    root.insert(3)
+    root.pre_order(root)
+
+    root.search(root, 1)
+    root.search(root, 4)
